565fad8a25e1e40d040228a6a0349a22fee9bc14
ellovna <ella.ellovna@gmail.com>
2023-06-06 06:16:01 -0400

    updated

--- LeftRightInnerOuter.sql
+++ LeftRightInnerOuter.sql
@@ -13,5 +13,17 @@ ON car_accident.car_id = car.car_id
 LEFT JOIN car_owner
 ON car_owner.car_id = car.car_id
 
+SELECT *
+FROM car RIGHT JOIN car_owner
+ON car.car_id = car_owner.car_id;
+
+SELECT *
+FROM car
+LEFT OUTER JOIN car_model
+ON car.car_id = car_model.car_id;
+
+
+
+
 
 
